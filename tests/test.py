2dc9823b4d772166c035824c0137c646a0bfe3cf
kaien07 <160471571+kaien07@users.noreply.github.com>
2025-04-04 03:50:24 +0000

    [qa] add sample test case

--- tests/test.py
+++ tests/test.py
@@ -1,1 +1,12 @@
+import unittest
 
+def validate_email(email):
+    return True
+
+class validate_email_tc(unittest.TestCase):
+    def test_at_symbol(self):
+        self.assertTrue(validate_email('kenneth@gmail.com'))
+        self.assertFalse(validate_email('kennethgmail.com'))
+
+if __name__ == '__main__':
+    unittest.main()
