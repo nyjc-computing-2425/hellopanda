2f745e08aa3fe58e6288a80f0e6ad9df4f725c86
chxy75 <160509015+chxy75@users.noreply.github.com>
2025-04-04 04:50:54 +0000

    [qa] implement test for validation for email and password

--- tests/test.py
+++ tests/test.py
@@ -1,12 +1,43 @@
 import unittest
+import validate
 
-def validate_email(email):
-    return True
-
-class validate_email_tc(unittest.TestCase):
+class TestValidateEmail(unittest.TestCase):
     def test_at_symbol(self):
-        self.assertTrue(validate_email('kenneth@gmail.com'))
-        self.assertFalse(validate_email('kennethgmail.com'))
+        self.assertTrue(validate.email('hellopanda@gmail.com'))
+        self.assertFalse(validate.email('hellopandagmail.com'))
+        self.assertFalse(validate.email("hellopanda#gamil.com"))
+
+    def test_no_space(self):
+        self.assertTrue(validate.email("hellopanda@gmail.com"))
+        self.assertFalse(validate.email("hello panda@gmail.com"))
+
+    def test_in_domain(self):
+        self.assertTrue(validate.email("hellopanda@gmail.com"))
+        self.assertFalse(validate.email("hellopanda@gmailcom"))
+
+class TestValidatePassword(unittest.TestCase):
+    def test_no_space(self):
+        self.assertTrue(validate.password("@R3alPassword"))
+        self.assertFalse(validate.password("a real password"))
+
+    def test_length(self):
+        self.assertTrue(validate.password("@R3alPassword"))
+        self.assertTrue(validate.password("R3alPassw@rd"))
+        self.assertFalse(validate.password("@R3alPass"))
+
+    def test_uppercase(self):
+        self.assertTrue(validate.password("@R3alPassword"))
+        self.assertFalse(validate.password("arealpassword"))
+
+    
+    def test_number(self):
+        self.assertTrue(validate.password("@R3alPassword"))
+        self.assertFalse(validate.password("aRealPassword"))
+
+    def test_special_char(self):
+        self.assertTrue(validate.password("@R3alPassword"))
+        self.assertFalse(validate.password('aR3alPassword'))
+
 
 if __name__ == '__main__':
     unittest.main()
