2de0b681946da78a0a52257e260f5c67b586702e
kaien07 <160471571+kaien07@users.noreply.github.com>
2025-04-04 04:44:29 +0000

    [qa] implement validation functions

--- validate.py
+++ validate.py
@@ -1,24 +1,24 @@
-def validate_email(email):
-    if email.count('@') != 1:
+def email(email_):
+    if email_.count('@') != 1:
         return False
-    if ' ' in email:
+    if ' ' in email_:
         return False
-    name, domain = email.split('@')
+    name, domain = email_.split('@')
     if '.' not in domain:
         return False
     
     return True
 
-def validate_password(password):
-    if ' ' in password:
+def password(password_):
+    if ' ' in password_:
         return False
-    if len(password) < 12:
+    if len(password_) < 12:
         return False
-    if password.lower() == password:
+    if password_.lower() == password_:
         return False
     contains_number = False
     contains_symbol = False
-    for char in password:
+    for char in password_:
         if char in '0123456789':
             contains_number = True
         if char in '!@#$%^&*()-=_+[]\\{}|;\':",./<>?':
@@ -28,4 +28,23 @@ def validate_password(password):
     if not contains_symbol:
         return False
       
+    return True
+
+def name(name_):
+    for char in name_:
+        if char in '0123456789':
+            return False
+        if char in '!@#$%^&*()=_+[]\\{}|;\':",./<>?':
+            return False
+        
+    return True
+
+def class_number(class_: int):
+    class_ = str(class_)
+    if len(class_) != 4:
+        return False
+    for char in class_:
+        if char not in '0123456789':
+            return False
+        
     return True
