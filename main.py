6bae3bdd6a6dde952d55280442e467d5ddc7e48d
UrgedReaper34 <shaneyeo109@gmail.com>
2025-04-09 22:25:23 +0800

    Cleanup

--- main.py
+++ main.py
@@ -5,22 +5,27 @@ app = Flask(__name__)
 
 @app.route('/') # Sprint 2 / MVP
 def index(): 
-    return render_template("pages/index.html")
+    return render_template("pages/index/index.html")
     
 
 @app.route('/student') # Sprint 2 / MVP
 def student_page():
-    return render_template('student.html')
+     # TODO: 
+    # 1. Get function from backend to get all events
+    # 2. Pass this data to render template
+     # (Optional) Create placeholder data on our end and pass it to render_template for testing
+    
+    return render_template('pages/student/student.html')
     
 
 @app.route('/student/event_details')
 def student_event_details_page():
-    return render_template("studenteventdetails.html")
+   return render_template("pages/studenteventdetails/studenteventdetails.html")
     
 
 @app.route('/login') # Sprint 2 / MVP
 def login_page():
-    return render_template('login.html')
+    return render_template('pages/login/login.html')
 
 
 @app.route('/organiser')
@@ -39,5 +44,5 @@ def organiser_create_event_page():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
 
