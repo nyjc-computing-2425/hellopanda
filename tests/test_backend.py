336189e17c6e5a6aee28b82527e739e73f8fb2cc
kaien07 <160471571+kaien07@users.noreply.github.com>
2025-04-10 05:49:01 +0000

    [qa] wrote unfinished test for backend storing accounts

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -8,14 +8,21 @@ if project_root not in sys.path:
 import unittest
 import sqlite3
 
-# import backend
+import backend
 
 class TestStoreAccountData(unittest.TestCase):
     def test_(self):
-        # conn = sqlite3.connect('test.db')
-        # cursor = conn.cursor()
-        # conn.execute(backend.sql.CREATE_TABLE_ACCOUNT)
-        # conn.commit()
-        self.assertEqual('abc', 'abc')
+        conn = sqlite3.connect('test.db')
+        cursor = conn.cursor()
+        conn.execute(backend.sql.CREATE_TABLE_ACCOUNT)
+        conn.execute(backend.sql.INSERT_INTO_ACCOUNT, ['abc@gmail.com', 'abcde', '@R3alPassword', 'student', 'Jonathan Lim', 2426, 2025]) # [email, salt, password, _type, name, _class, graduation_year]
+        conn.commit()
+        result_cursor = conn.execute("SELECT * FROM account")
+        rows = result_cursor.fetchall()
+        print([dict(row) for row in rows])
+        conn.execute(backend.sql.DELETE_TABLE_ACCOUNT)
+        conn.commit()
+        conn.close()
 
-        
+if __name__ == "__main__":
+    unittest.main()
