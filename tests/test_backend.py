c2de93bc87016a57a08d9ece8f7f68ccee8e8be5
kaien07 <160471571+kaien07@users.noreply.github.com>
2025-04-10 05:53:28 +0000

    [qa] finish testing for backend storing of account data

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -12,16 +12,19 @@ import backend
 
 class TestStoreAccountData(unittest.TestCase):
     def test_(self):
-        conn = sqlite3.connect('test.db')
-        cursor = conn.cursor()
+        conn = sqlite3.connect('tests/test.db')
+        
         conn.execute(backend.sql.CREATE_TABLE_ACCOUNT)
         conn.execute(backend.sql.INSERT_INTO_ACCOUNT, ['abc@gmail.com', 'abcde', '@R3alPassword', 'student', 'Jonathan Lim', 2426, 2025]) # [email, salt, password, _type, name, _class, graduation_year]
         conn.commit()
-        result_cursor = conn.execute("SELECT * FROM account")
-        rows = result_cursor.fetchall()
-        print([dict(row) for row in rows])
+
+        cursor = conn.execute("SELECT * FROM account")
+        rows = cursor.fetchall()
+        self.assertEqual(list(rows[0]), ['abc@gmail.com', 'abcde', '@R3alPassword', 'student', 'Jonathan Lim', 2426, 2025])
+        
         conn.execute(backend.sql.DELETE_TABLE_ACCOUNT)
         conn.commit()
+        
         conn.close()
 
 if __name__ == "__main__":
