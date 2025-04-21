d6ce385be2759f1f11560625ce277fb0986014d5
yongteck <160562805+yongteck@users.noreply.github.com>
2025-04-21 06:52:10 +0000

    made an /organiser page

--- main.py
+++ main.py
@@ -18,9 +18,6 @@ def index():
 def student_page():
     return render_template('/pages/student/student.html', events= [{"id":"blm day", "topic":"gimme fried chicken"},
                                                                    {"id":"reverse blm day", "topic":"steal my fried chicken"}])
-@app.route('/student/event_details')
-def student_details_page():
-    return render_template('/pages/studenteventdetails/studenteventdetails.html')
 
 @app.route('/login', methods = ["GET", "POST"]) # Sprint 2 / MVP
 def login_page():
@@ -52,7 +49,8 @@ def login_page():
 
 @app.route('/organiser')
 def organiser_page():
-    return "organiser home page"
+    return render_template("pages/organiser/organiser.html", events=[{"id":"ny fiesta", "topic":"throw pch into the water"},
+                                                                     {"id":"sigma day", "topic":"chicken jockey"}])
 
 
 @app.route('/organiser/events')
@@ -83,6 +81,10 @@ def privacy_page():
 @app.route("/terms")
 def terms_page():
     return render_template("pages/terms/terms.html")
+
+@app.route("/flag.txt")
+def flag():
+    return "CTFSG{P4NG_P4NG_TH4_G0aT}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
