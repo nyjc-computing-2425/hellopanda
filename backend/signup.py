ae94a06e9d152d42ca3a9eac20434a6e689e8807
Joshua <132035532+Kailenator-AZH@users.noreply.github.com>
2025-04-22 13:37:18 +0000

    Add create_signup_table and delete_signup_table functions to manage signup table

--- backend/signup.py
+++ backend/signup.py
@@ -1,1 +1,19 @@
 from .base import execute_query
+
+####################################SIGNUP#################################
+def create_signup_table() -> None:
+    """
+    Creates the account table
+    """
+    execute_query("""
+        CREATE TABLE IF NOT EXISTS "signup" (
+            "email" TEXT NOT NULL,
+            "event_id" INTEGER NOT NULL
+        );
+    """)
+
+#Delete account table
+def delete_signup_table():
+    execute_query("""
+        DROP TABLE IF EXISTS signup;
+    """)
