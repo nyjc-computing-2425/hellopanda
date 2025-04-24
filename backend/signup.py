eab3e284a9bdf4de2f8ade11dd9e8e8741efeb99
kaydnnn <160500192+kaydnnn@users.noreply.github.com>
2025-04-24 13:23:04 +0800

    Update signup.py

--- backend/signup.py
+++ backend/signup.py
@@ -14,8 +14,8 @@ def delete_signup_table():
 
 #retrieve events signed up
 def get_signed_up_events(email):
-    dic = execute_query(sql.GET_SIGN_UP_EVENT, [email])
-    return [row["event_id"] for row in dic]
+    results = execute_query(sql.GET_SIGN_UP_EVENT, [email])
+    return [row["event_id"] for row in results]
 
 def add_student_to_event(email, event_id):
     execute_query(sql.ADD_STUDENT_TO_EVENT , [email, event_id])
@@ -24,8 +24,17 @@ def remove_student_from_event(email, event_id):
     execute_query(sql.REMOVE_STUDENT_FROM_EVENT, [email, event_id])
 
 def get_event_participants(event_id):
-    dic = execute_query(sql.GET_EVENT_PARTICIPATION, [event_id])
-    return [row["email"] for row in dic]
+    results = execute_query(sql.GET_EVENT_PARTICIPATION, [event_id])
+    return [row["email"] for row in results]
 
 def mark_attendance(email, event_id):
     execute_query(sql.MARK_ATTENDENCE,[email, event_id])
+
+def is_present(email, event_id):
+    results = execute_query("""
+        SELECT "attendance" FROM signup
+        WHERE email = ? AND event_id = ?
+        
+    """, [email, event_id])
+    return bool(results[0]["attendance"])
+
