06659bf6f9dbe03c2f43bd2a06298bc5f1379b66
Joshua <132035532+Kailenator-AZH@users.noreply.github.com>
2025-04-24 13:00:25 +0000

    update comments in backend test script

--- run_backend_tests.py
+++ run_backend_tests.py
@@ -12,28 +12,27 @@ def initialise():
 
     ########Dummy data##########
     account.store_account_data("notjohn@gmail.com", "JuWMqIugOlEtuXLC", "40493376214957570934363042765919812022617836654205352252256196165357072291861", "student", "hehe", 2426, 2025)
-    #password salty
+    #email: notjohn@gmail.com password: salty
     event.store_event_data(1, "2025-06-12 09:00:00", "2025-06-13 09:00:00", "Bad event", "hi  imagine events", "Home")
     event.store_event_data(2, "2025-04-10 09:00:00", "2025-06-13 09:00:00", "Another event", "HAHA!", "Probably in sch")
     signup.add_student_to_event("notjohn@gmail.com", 1)
 
 initialise()
-
 ###############ACCOUNT######################
-print("Account")
-account.update_class("notjohn@gmail.com", 2426)
-account.update_year("notjohn@gmail.com", 1000)
-print(account.retrieve_byclass(2426))
-print(account.acc_type("john@gmail.com"))
+# print("Account")
+# account.update_class("notjohn@gmail.com", 2426)
+# account.update_year("notjohn@gmail.com", 1000)
+# print(account.retrieve_byclass(2426))
+# print(account.acc_type("john@gmail.com"))
 
 ###################EVENT######################
-print("Event")
-event.store_event_data(1, "2025-06-12 09:00:00", "2025-06-13 09:00:00", "Bad event", "hi  imagine events", "Home")
-event.store_event_data(2, "2025-04-10 09:00:00", "2025-06-13 09:00:00", "Another event", "HAHA!", "Probably in sch")
-print(event.retrieve_all_events())
-print("Upcoming: ",event.retrieve_upcoming_events())
-print("Current: ",event.retrieve_current_events())
-print(event.retrieve_byname("bad event"))
+# print("Event")
+# event.store_event_data(1, "2025-06-12 09:00:00", "2025-06-13 09:00:00", "Bad event", "hi  imagine events", "Home")
+# event.store_event_data(2, "2025-04-10 09:00:00", "2025-06-13 09:00:00", "Another event", "HAHA!", "Probably in sch")
+# print(event.retrieve_all_events())
+# print("Upcoming: ",event.retrieve_upcoming_events())
+# print("Current: ",event.retrieve_current_events())
+# print(event.retrieve_byname("bad event"))
 
 #################SIGNUP#########################
 # print("Signup")
